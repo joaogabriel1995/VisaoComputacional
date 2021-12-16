60eac89eda747a8b270d401cccbb35190eb45cf7
joaogabriel1995 <joaogabrielrp@gmail.com>
2021-12-16 00:21:13 -0300

    Script detecção e bounding box

--- OCR-POST1/SelecaoTexto.py
+++ OCR-POST1/SelecaoTexto.py
@@ -9,26 +9,25 @@ import numpy as np
 
 class BoxTexto:
 
-    def __init__(self, img_path, nome_janela="Imagem"):
+    def __init__(self, img_path):
 
         self.img_path = img_path
-        self.nome_janela =  nome_janela
         self.Imagem = cv2.imread(self.img_path)
 
 
-    def visualizar_img(self):
+    def visualizar_img(self, nome_janela="Imagem"):
 
-        cv2.imshow(str(self.nome_janela), self.Imagem)
+        cv2.imshow(str(nome_janela), self.Imagem)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 
     def escreve_texto(self, texto, x, y, img, fonte=r"D:\Arquivos Pessoais\Faculdade\Python\Tesseract\Fontes\calibri.ttf", tamanho_texto=32):
-        
+
         fonte = ImageFont.truetype(fonte, tamanho_texto) 
         img_pil = Image.fromarray(img) #estamos trabalhando com um tipo numpy.ndarray precisamos converter para o formato do PIL imagem.
         draw = ImageDraw.Draw(img_pil) #Passamos a imagem que queremos fazer o desenho em cima.
-        draw.text((x, y - tamanho_texto), texto, font=fonte) #escolhemos a posição x, a posição y deslocamos o tamanho do texto para ficar uniforme, texto e fonte
+        draw.text((x, y - tamanho_texto), texto, font=fonte, fill="#000") #escolhemos a posição x, a posição y deslocamos o tamanho do texto para ficar uniforme, texto e fonte
         img = np.array(img_pil) #transformamos em um formato np.array
         return img
 
@@ -37,6 +36,7 @@ class BoxTexto:
     def draw_box(self, confiança=40, cor=(255,100,0)):
 
         self.resultado = ocr.image_to_data(self.Imagem, lang="por", output_type=Output.DICT)
+        print(self.resultado)
         array = []
         img = self.Imagem.copy()
         for i in range(0,len(self.resultado["text"])):
@@ -62,14 +62,10 @@ class BoxTexto:
 
     
     
-    
-
-
-
 
 if __name__=="__main__":
     
-    image_path = r"D:\Arquivos Pessoais\Faculdade\Python\Tesseract\imagens\teste02.jpg"
+    image_path = r"D:\Arquivos Pessoais\Faculdade\Python\Tesseract\imagens\Post2-imagem1.png"
     imagem1 = BoxTexto(image_path)
     imagem1.visualizar_img()
     imagem1.draw_box()
